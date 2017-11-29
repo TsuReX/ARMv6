3ac82419246ae4a8eb7a363bd5757f5178086609
vasily.v.yurchenko <vasily.yurchenko@gmail.com>
2017-11-29 11:08:28 +0300

    Functions readstr, printstr and calclen were moved from program.s into new file function.s. The file functions.s was included into program.s

--- src/program.s
+++ src/program.s
@@ -1,4 +1,7 @@
 ; The program was written with NASM language
+
+%include 'src/functions.s'
+
 section .somesect
 	somedata dq 0xAABBCCFF
 
@@ -32,41 +35,6 @@ section .text					;section declaration
 								;we must export the entry point to the ELF linker or
 	global  entryPoint			;loader. They conventionally recognize _start as their
 								;entry point. Use ld -e foo to override the default.
-
-;*************************************************
-
-readstr: ;function
-
-	mov		edx, [rsp + 0x8]	;number of bytes to read
-	mov		ecx, [rsp + 0x10]	;reserved space to store our input (known as a buffer)
-	mov		ebx, 0				;write to the STDIN file
-	mov		eax, 3				;invoke SYS_READ (kernel opcode 3)
-	int		80h
-	ret
-
-;*************************************************
-
-printstr: ;function				;write our string to stdout
-	mov		rcx, [rsp+0x8]		;get first operand value from stack
-	mov		rdx, [rsp+0x10]		;get second operand value from stack
-    mov		rbx,1				;first argument: file handle (stdout)
-    mov		rax,4				;system call number (sys_write)
-    int		0x80				;call kernel, return value is stored in rax
-	ret							;return from printstr, return value shuld be stored in rax
-
-;*************************************************
-
-calclen: ;function
-	mov		rax, [rsp+0x8]		;get operand value from stack
-	mov		rbx, rax
-.nextchar:						;"." before name of label means that this labes is local for calclen function
-    cmp		byte [rax], 0
-    jz		.finished
-    inc		rax
-    jmp		.nextchar
-.finished:
-	sub		rax, rbx
-	ret							;return from calclen, return value shuld be stored in rax
 
 ;*************************************************
 
