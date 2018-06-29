70b9bd479ff33bd02ba4958c6b47bf40dc0863bd
Vasily V. Yurchenko <vasily.v.yurchenko@mcst.com>
2018-06-29 19:34:18 +0300

    IRQ setup routines were implemented. Tested on QEMU.

--- bios/arm/start.s
+++ bios/arm/start.s
@@ -12,27 +12,54 @@ entry:
     bl setup_stack
     
     bl setup_irq
+
+    bl enable_irq
     
-    bl test_gpio
+    bl setup_gpio
+
+    bl enable_timer_irq
+
+    bl enable_timer
     
     b .
 
-test_gpio:
+
+enable_irq:
+    mrs r0, cpsr
+    ldr r1, =0xFFFFFF7F
+    and r0, r1
+    msr cpsr, r0
+    mov pc, lr
+
+switch_led:
+    ldr r0, =0x7E200010
+    ldr r1, [r0] 
+    mov r2, #0x10000
+    eor r1, r2
+    str r1, [r0]
+    mov pc, lr
+
+
+setup_gpio:
     //Select GPIO 16 as output     
     mov r0, #0x1
     mov r0, r0, lsl #0x12 @ mov r0, 0x40000
     //GPFSEL1
-    ldr r1, =0x3E200004
+    //ldr r1, =0xFFFFFFC
+    ldr r1, =0x7E200004
     str r0, [r1]
     
+    mov pc, lr
     
     mov r0, r0, lsr #0x2
     //GPSET0
-    ldr r1, =0x3E200010
+    //ldr r1, =0x10000000
+    ldr r1, =0x7E200010
     str r0, [r1]
     
     //GPCLR1
-    ldr r1, =0x3E200028
+    //ldr r1, =0x8
+    ldr r1, =0x7E200028
     str r0, [r1]
     
     mov pc, lr
@@ -55,6 +82,32 @@ setup_stack:
 
     mov pc, lr
 
+enable_timer:
+    ldr r0, =0x7E0B0400
+    ldr r1, =0xF4240
+    str r1, [r0]	@ Load 1.000.000 into the counter register
+    ldr r0, =0x7E0B0408
+    ldr r1, [r0]
+    mov r2, #0xA2
+    orr r1, r2
+    str r1, [r0]	@ Set Timer enable, Timer interrupt enable, 23-bit counter set
+    mov pc, lr
+
+enable_timer_irq:
+    ldr r0, =0x7E0B0218
+    ldr r1, [r0]
+    orr r1, #0x1
+    str r1, [r0]
+    mov pc, lr
+
+disable_timer_irq:
+    ldr r0, =0x7E0B0218
+    ldr r1, [r0]
+    and r1, #0xFFFFFFFE
+    str r1, [r0]
+    mov pc, lr
+
+
 setup_irq:
     ldr r11, =0x00000000
     ldr r10, =ivt_start
@@ -70,7 +123,7 @@ err_handler:
     b .
 
 irq_handler:
-
+    bl switch_led
     subs pc, lr, #0x4
 
 
