c7201c9a5bf02da28e3a0224bee23a395c7cae17
Vasily V. Yurchenko <vasily.v.yurchenko@mcst.com>
2018-07-23 19:11:36 +0300

    UART setup, receive and send functions were implemented. Not tested.

--- bios/arm/start.s
+++ bios/arm/start.s
@@ -10,25 +10,50 @@ entry:
     orr r0, #0x1F    
     msr cpsr, r0
     
+    bl setup_stack
     bl read_ns_bit
-
     bl setup_gpio
+    bl setup_irq_vector
+    bl enable_irq
+    @bl enable_timer_irq
+    @bl enable_timer
+
+    bl blink_led
     
-    bl gpio_16_0
+    b .
 
+//******************************************************************//
+
+blink_led:
+    str lr, [sp], #-0x4
+
+	bl wait_10m
+
+	bl gpio_16_0
     bl wait_10m
-
     bl gpio_16_1
-    
+
     bl wait_10m
 
+    bl gpio_24_0
+    bl wait_10m
     bl gpio_24_1
 
-    bl wait_10m
+	bl wait_10m
 
+    bl gpio_25_0
+	bl wait_10m
     bl gpio_25_1
 
-    ldr r0, =0x20200000
+	bl wait_10m
+
+    bl gpio_16_0
+    bl gpio_24_0
+    bl gpio_25_0
+
+	//*****************************
+
+	ldr r0, =0x20200000
     ldr r2, =0x10000
     ldr r1, [r0, #0x1C]
     and r1, r2
@@ -43,62 +68,17 @@ entry:
     and r1, r2
     str r1, [r0, #0x28]
 
-    
 
-    bl gpio_24_1
+    bl gpio_16_sw
+	bl wait_10m
+    bl gpio_24_sw
+	bl wait_10m
+    bl gpio_25_sw
 
-    bl gpio_25_1
+	//*****************************
 
-    
-    bl gpio_16_1
-
-    bl gpio_24_0
-
-    bl gpio_25_0
-
-
-    bl gpio_16_0
-
-    bl gpio_24_1
-
-    bl gpio_25_1
-
-    
-    //bl gpio_16_sw
-
-    //bl gpio_24_sw
-
-    //bl gpio_25_sw
-
-
-    b .
-
-    bl setup_stack
-    
-    bl setup_irq
-
-    bl enable_irq
-    
-    bl enable_timer_irq
-
-    bl enable_timer
-    
-    b .
-
-
-read_ns_bit:
-    mcr p15, 0, r0, c1, c1, 0
+    ldr lr, [sp, #0x4]!
     mov pc, lr
-
-wait_10m:
-    str r0, [sp], #-0x4
-    ldr r0, =10000000
-loop:
-    subs r0, #0x1
-    bne loop
-    ldr r0, [sp, #0x4]!
-    mov pc, lr
-
 
 gpio_16_1:
     ldr r0, =0x20200000
@@ -169,12 +149,6 @@ gpio_25_sw:
     str r1, [r0, #0x28]
     mov pc, lr
 
-enable_irq:
-    mrs r0, cpsr
-    orr r0, #0x80
-    msr cpsr, r0
-    mov pc, lr
-
 switch_leds:
     str lr, [sp]
     bl gpio_16_sw
@@ -182,6 +156,78 @@ switch_leds:
     bl gpio_25_sw
     mov pc, lr
     ldr lr, [sp]
+
+//******************************************************************//
+
+read_ns_bit:
+    mcr p15, 0, r0, c1, c1, 0
+    mov pc, lr
+
+wait_10m:
+    str r0, [sp], #-0x4
+    ldr r0, =10000000
+loop:
+    subs r0, #0x1
+    bne loop
+    ldr r0, [sp, #0x4]!
+    mov pc, lr
+
+enable_irq:
+    mrs r0, cpsr
+    orr r0, #0x80
+    msr cpsr, r0
+    mov pc, lr
+
+setup_uart:
+
+	ldr r0, =0x20200000
+    ldr r1, =0x24000
+    ldr r2, [r0, #0x4]
+	orr r2, r2, r1
+	str	r2, [r0, #0x4] @ Enable function 0 for GPIO 14 and 15
+
+	ldr r0, =0x20201000
+
+	mov r1, #0x0
+	str r1, [r0, #0x30] @ Turn off UART
+
+	mov r1, #0x1
+	str r1, [r0, #0x24] @ Integer divisor 1
+
+	mov r1, #0x28
+	str r1, [r0, #0x28] @ Fraction divisor 40
+
+	mov r1, #0x70
+	str r1, [r0, #0x2C] @ Setup FIFO enabled and 8bit data length
+
+	mov r1, #0x301
+	str r1, [r0, #0x30] @ Turn on UART, Transmitter and Receiver
+
+	mov pc, lr
+
+uart_recv:
+	ldr r1, =0x20200000
+
+	loop:
+	ldr r0, [r1, #0x18]
+	and r0, r0, #0x10
+	bne loop
+
+	ldr r0, [r1]
+
+	mov pc, lr
+
+uart_send:
+	ldr r1, =0x20200000
+
+	str r0, [r1]
+
+	loop:
+	ldr r0, [r1, #0x18]
+	and r0, r0, #0x20
+	bne loop
+
+	mov pc, lr
 
 setup_gpio:
     ldr r0, =0x20200000
@@ -232,8 +278,7 @@ disable_timer_irq:
     str r1, [r0]
     mov pc, lr
 
-
-setup_irq:
+setup_irq_vector:
     ldr r11, =0x00000000
     ldr r10, =ivt_start
 
