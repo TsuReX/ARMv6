12b34cef6a5d69687115fa1ab9bfcfec0f661ea3
Vasily Yurchenko <vasily.yurchenko@gmail.com>
2018-09-10 15:23:13 +0300

    Some mistakes were fixed, comments were added.

--- bios/arm/uart.s
+++ bios/arm/uart.s
@@ -4,11 +4,11 @@ setup_uart:
 	@ GPIO alt function 0(0b100) for 14 and 15 
 	
 	ldr r0, =0x20200000 	@ GPIO base address
-	ldr r1, =0x24000 	@ GPIO alt function 0(0b100) for 14 and 15 
+	ldr r1, =0x24000		@ GPIO alt function 0(0b100) for 14 and 15
 	
-	ldr r2, [r0, #0x4] 	@ GPIO function select 1
-	orr r2, r2, r1 		@ Change state only for 14 and 15 GPIOs
-	str r2, [r0, #0x4] 	@ Enable function 0 for GPIO 14 and 15
+	ldr r2, [r0, #0x4]		@ GPIO function select 1
+	orr r2, r2, r1			@ Change state only for 14 and 15 GPIOs
+	str r2, [r0, #0x4]		@ Enable function 0 for GPIO 14 and 15
 	
 	
 	@ Setup UART on 14(TX) and 15(RX) GPIOs
@@ -27,19 +27,24 @@ setup_uart:
 	ldr r1, =0x7FF
 	str r1, [r0, #0x44] 	@ Clear pending interrupts
 
-	mov r1, #0x70 		@ Set 1 into 4,5,6 bits
+	mov r1, #0x70			@ Set 1 into 4,5,6 bits
 	str r1, [r0, #0x2C] 	@ Setup FIFO enabled and 8bit data length
 	
-	ldr r1, =0x7F2 		@ Set 1 into 1,4-10 bits
+	ldr r1, =0x7F2			@ Set 1 into 1,4-10 bits
 	str r1, [r0, #0x38] 	@ Mask all interrupts
 
-	ldr r1, =0x301 		@ Set 1 into 0,8,9 bits
+	ldr r1, =0x301			@ Set 1 into 0,8,9 bits
 	str r1, [r0, #0x30] 	@ Enable UART, transmitter and receiver
 
 	mov pc, lr
 
+/*	Receive byte via UART
+ *	Input:
+ *	Return:	r0 - data
+ 			r2 - error code
+**/
 uart_recv:
-	str r0, [r13], #-0x4
+	@str r0, [r13], #-0x4
 	str r1, [r13], #-0x4
 
 	ldr r1, =0x20200000
@@ -47,17 +52,22 @@ uart_recv:
 	1:			@ Wait while recv FIFO is empty
 	ldr r0, [r1, #0x18]	@ Read recv FIFO empty flag
 	and r0, r0, #0x10	@ Check flag
+	@ TODO Exit after 100 attempts
 	bne 1b
-
+	@ TODO Fill r2 with error cdoe
 	ldr r0, [r1]		@ Load data from FIFO into r0
 
 	ldr r1, [r13, #0x4]!
-	ldr r0, [r13, #0x4]!
+	@ldr r0, [r13, #0x4]!
 
 	mov pc, lr
 
+/*	Send byte via UART
+ *	Input:	r0 - data
+ *	Return:	r0 - error code
+**/
 uart_send:
-	str r0, [r13], #-0x4
+	@str r0, [r13], #-0x4
 	str r1, [r13], #-0x4
 
 	ldr r1, =0x20200000
@@ -67,9 +77,10 @@ uart_send:
 	1:			@ Wait while recv FIFO is empty
 	ldr r0, [r1, #0x18]	@ Read trans FIFO full flag
 	and r0, r0, #0x20	@ Check flag
+	@ TODO Exit after 100 attempts
 	bne 1b
 
 	ldr r1, [r13, #0x4]!
-	ldr r0, [r13, #0x4]!
+	@ldr r0, [r13, #0x4]!
 
 	mov pc, lr
