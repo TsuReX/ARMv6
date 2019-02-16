6fde39a5ac3007e0a5ba1edabe85f005d9b9c086
Vasily Yurchenko <vasily.yurchenko@gmail.com>
2019-02-16 12:47:18 +0300

    The top CMakeLists.txt now has C and ASM compiler flags. stub.c was modified for experiments.

--- bios/common/stub.c
+++ bios/common/stub.c
@@ -1,3 +1,6 @@
-int stub_func() {
-    return 0xDEADBEEF;
+#include "stdint.h"
+
+uint32_t stub_func(uint32_t val1, uint32_t val2, uint32_t val3/*, uint32_t val4, uint32_t val5*/) {
+	val1++;	
+	return val1 + val2 + val3 /*+ val4 + val5*/;
 }
