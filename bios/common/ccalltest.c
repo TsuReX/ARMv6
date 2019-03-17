408a0079f2f5b1c98f20a4b2f5522a15d34a04c0
Vasily Yurchenko <vasily.yurchenko@gmail.com>
2019-03-17 17:50:48 +0300

    ccall_prologue and ccall_epilogue marcos were implemented. ccall was remade. check_ccall macro finishes successfuly. Work with regard to Issue #22 was finished.

--- bios/common/ccalltest.c
+++ bios/common/ccalltest.c
@@ -1,17 +1,35 @@
 #include "stdint.h"
 
-extern int32_t func(int32_t v0, int32_t v1, int32_t v2, int32_t v3, int32_t v4, int64_t v5, int64_t v6);
-
 uint32_t sum(uint32_t *array, uint32_t cnt) {
     if (cnt == 1)
         return array[0];
     return array[cnt - 1] + sum(array, cnt - 1);
 }
 
-c_test_1(uint32_t v0, uint32_t v1, uint32_t v2, uint32_t v3, uint32_t v4, uint32_t v5, uint32_t v6) {
-   // uint32_t array[5] = {v0, v1, v2, v3, v4};
-    uint32_t a = v6 + v5;
-    return func(v0, v1, v2, v3, v4, v5, a);
-   // return sum(array, 5);
+uint64_t c_test_1(	uint32_t *arg_array_src, uint32_t *arg_array_dst, 
+					uint32_t a2, uint32_t a3, 
+					uint64_t a4, uint32_t a5,
+					uint64_t a6, uint32_t a7) {
+	
+	((uint64_t*)arg_array_dst)[0] = a4;
+	((uint64_t*)arg_array_dst)[1] = a5;
+	((uint64_t*)arg_array_dst)[2] = a6;
+	((uint64_t*)arg_array_dst)[3] = a7;
+	
+	return (uint64_t)a3 << 32 | a2;
+	
 }
 
+uint32_t c_test_2(	uint32_t *arg_array_src,
+					uint32_t *arg_array_dst, 
+					uint32_t *arg_array_res,
+					uint32_t wordscount) {
+	while (wordscount > 0) {
+		
+		if (arg_array_dst[wordscount - 1] != arg_array_res[wordscount - 1])
+			return 0xDEADBEEF;
+		--wordscount;
+	}
+	return 0xBABADEDA;
+	
+}
--- bios/common/stub.c
+++ bios/common/stub.c
@@ -1,10 +1,10 @@
 #include "stdint.h"
 
-uint32_t stub_func(uint32_t val1, uint32_t val2, uint32_t val3/*, uint32_t val4, uint32_t val5*/) {
-	val1++;	
-	return val1 + val2 + val3 /*+ val4 + val5*/;
-}
-
-int32_t func(int32_t v0, int32_t v1, int32_t v2, int32_t v3, int32_t v4, int32_t v5, int32_t v6, int32_t v7) {
+int32_t stub_func(int32_t v0, int32_t v1,
+				  int32_t v2, int32_t v3,
+				  int32_t v4, int32_t v5,
+				  int32_t v6, int32_t v7) {
+	
 	return v0 + v1 + v2 + v3 + v4 + v5 + v6 + v7;
+
 }
