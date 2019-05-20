1122af13571c5979938db63090e37baa25ce0ca0
Vasily Yurchenko <vasily.yurchenko@gmail.com>
2019-05-20 15:14:41 +0300

    led() function was implemented.

--- bios/common/post.c
+++ bios/common/post.c
@@ -10,14 +10,23 @@
 #define GPIO_POST_BIT_COUNT	0x4
 
 void led(uint32_t led_num, uint32_t on) {
-
+	
+	uint32_t ret_val = 0;
+	asm volatile ( 
+		"mov r0, %1 \t\n"
+		"mov r1, %2 \t\n"
+		"bl gpio_set \t\n"
+		"mov %0, r0 \t\n" 
+		:"=r"(ret_val) :"r"(led_num), "r"(on)
+	);
 }
 
 void gpio_post(uint32_t post_val) {
 
 	uint32_t i = 0;
+	uint32_t map[GPIO_POST_BIT_COUNT] = {0, 1, 2, 3, 4, 5, 6, 7};
 	for(; i < GPIO_POST_BIT_COUNT; ++i) {
-		led(i, (post_val >> i) & 1);
+		led(map[i], (post_val >> i) & 1);
 	}
 }
 
