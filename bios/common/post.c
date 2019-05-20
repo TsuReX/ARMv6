c2f9f327b6514367a934f2b675eeca97edd31dac
Vasily Yurchenko <vasily.yurchenko@gmail.com>
2019-05-20 10:57:15 +0300

    Function gpio_init_rpib() was implemented but initializes GPIO templates. Templates should be changed by real GPIO numbers. gpio_post() was implemented. It call led() function which must be implemented by inline assembly language. gpio_init_rpib() inserted in start(). Header file gpio.S was renamed into gpio_rpib.inc.S. Non fuction code modification was made. Not tested.

--- bios/common/post.c
+++ bios/common/post.c
@@ -1,14 +1,24 @@
-#include "stdint.h"
+#include <stdint.h>
+#include <post.h>
 
-#include "post.h"
+#define GPIO_POST_MASK		0x01
+#define UART_POST_MASK		0x02
+#define I2C_POST_MASK		0x04
 
-#define GPIO_POST_MASK	0x01
-#define UART_POST_MASK	0x02
-#define I2C_POST_MASK	0x04
+#define POST_MASK 			GPIO_POST_MASK
 
-#define MASK GPIO_POST_MASK
+#define GPIO_POST_BIT_COUNT	0x4
+
+void led(uint32_t led_num, uint32_t on) {
+
+}
 
 void gpio_post(uint32_t post_val) {
+
+	uint32_t i = 0;
+	for(; i < GPIO_POST_BIT_COUNT; ++i) {
+		led(i, (post_val >> i) & 1);
+	}
 }
 
 void uart_post(uint32_t post_val) {
@@ -18,21 +28,25 @@ void i2c_post(uint32_t post_val) {
 }
 
 void post(uint32_t post_val) {
-	
-	if (MASK == 0) {
+
+	if (POST_MASK == 0) {
 		gpio_post(post_val); // Default post
 		return;
 	}
-	if (MASK & 0xF !=0) {
+
+	if (POST_MASK & 0xF !=0) {
 		gpio_post(post_val); 
 		uart_post(post_val);
 		i2c_post(post_val);
 		return;
 	}
-	if (MASK & UART_POST_MASK != 0)
+
+	if (POST_MASK & UART_POST_MASK != 0)
 		uart_post(post_val);
-	if (MASK & I2C_POST_MASK != 0)
+
+	if (POST_MASK & I2C_POST_MASK != 0)
 		i2c_post(post_val);
-	if (MASK & GPIO_POST_MASK != 0)
+
+	if (POST_MASK & GPIO_POST_MASK != 0)
 		gpio_post(post_val);
 }
